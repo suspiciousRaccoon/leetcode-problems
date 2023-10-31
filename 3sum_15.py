c36e20241e5d2c2a59d60fe8d51c1ab256ac28fa
suspiciousRaccoon <spablocabral@gmail.com>
2023-10-31 20:29:56 -0300

    Add 15. 3Sum

---
+++ 3sum_15.py
@@ -0,0 +1,26 @@
+from bisect import bisect_right
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            left, right = i+1, len(nums) - 1
+            while left < right:
+                three_sum = a + nums[left] + nums[right]
+                if three_sum > 0:
+                    right -= 1
+                elif three_sum < 0:
+                    left += 1
+                else:
+                    res.append([a, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        return res
