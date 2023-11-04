fd072301b2492a782b7627c24e0d53041bc3d678
suspiciousRaccoon <spablocabral@gmail.com>
2023-11-03 22:53:08 -0300

    Add 42. Trapping Rain Water

---
+++ trapping_rain_water_42.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        result = 0
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                result += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                result += right_max - height[right]
+        return result
