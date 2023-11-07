d240c3cf56d3333a7b58a3e8319563fa67d38bfc
suspiciousRaccoon <spablocabral@gmail.com>
2023-11-07 09:37:52 -0300

    Add bisec approach

--- binary_search_704.py
+++ binary_search_704.py
@@ -1,3 +1,4 @@
+import bisect
 from typing import List
 from utils import binary_search
 
@@ -5,3 +6,7 @@ from utils import binary_search
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         return binary_search(nums, target)
+
+    def search_bisec(self, nums: List[int], target: int) -> int:
+        index = bisect.bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
